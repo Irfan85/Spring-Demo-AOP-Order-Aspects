8288286bb2e00a16b36cc7aa03a9cc96d3d2d998
Irfan Muhit <irfan.muhit85@gmail.com>
2022-07-04 12:35:49 +0600

    Ordering aspects

--- src/com/example/aopdemo/aspect/DemoLoginAspect.java
+++ src/com/example/aopdemo/aspect/DemoLoginAspect.java
@@ -2,10 +2,12 @@ package com.example.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class DemoLoginAspect {
 
     // Reusing a pointcut expression that was declared before 
--- src/com/example/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/com/example/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.example.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
     @Before("com.example.aopdemo.aspect.MyAopExpressions.forDAOPackageNoGetterSetter()")
     public void performAPIAnalytics() {
